f296d23ecb9eaf194a0abbf552ee64ae6492abc9
alfinorossesa <alfino.zxcvbnm@gmail.com>
2022-07-11 22:55:56 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <section class="container pt-4">
+        <img src="youtube-logo.png" alt="logo" width="130px">
+        <div style="border: 1px solid rgb(211, 210, 210); padding: 15px 50px 15px 50px;">
+            <p class="pb-2 mt-2"><strong>Please Subscribe!</strong></p>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card p-3 mb-4">
+                        <div class="g-ytsubscribe" data-channelid="UCeBev0IY3_mg3Tf8lpxhtig" data-layout="full" data-count="default"></div>
+                    </div>
+                    <div class="card p-3 mb-4">
+                        <div class="g-ytsubscribe" data-channelid="UCURzAONtAbFlm-j7cqOLKng" data-layout="full" data-count="default"></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-3 mb-4">
+                        <div class="g-ytsubscribe" data-channelid="UCtlHPNHDIOUU5xsH0ImW4Cw" data-layout="full" data-count="default"></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-3 mb-4">
+                        <div class="g-ytsubscribe" data-channelid="UCJqNJJr60JwIjEGVHAvz1cg" data-layout="full" data-count="default"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <!-- youtube sub button -->
+    <script src="https://apis.google.com/js/platform.js"></script>
+</body>
+</html>
